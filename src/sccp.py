d8018dac056e1c333e80cfcde8848ccf49343d38
Alban Puech <alban.puech@truite.polytechnique.fr>
2021-11-23 13:25:18 +0100

    update update_ev

--- src/sccp.py
+++ src/sccp.py
@@ -15,16 +15,36 @@ def fetch_temporaries():
 def initialize_conditions(cfg: CFG):
     '''Initialise the maps Val and Ev'''
     val = {r: Constants.unused for r in fetch_temporaries(cfg)}
-    ev = {b: False for b in cfg.blocks}
-    return val, ev
+    ev = {b.label: (True if b.label == cfg.lab_entry else False)  for b in cfg._blockmap}
+    return ev, val
 
 def update_ev(cfg: CFG, ev: dict, val: dict):
     '''Update the map ev'''
-    pass
+    change = False
+    for label, block in cfg._blockmap :
+        definite_jump = False 
+        for jump in block.jumps :
+            jump_type = jump.opcode
+            if  jump_type == "jmp" and not definite_jump:
+                ev["label"] = True 
+            else :
+                x = jump.arg1 :
+                if val[x] == Constants.non_constant :
+                    ev["label"] = True 
+                if val[x] == Constants.unused :
+                    ev["label"] = True 
+                else :
+                    ## check according to type of jump and value of the temporary
+                    definite_jump = True 
+
+
+
+    return change 
 
 def update_val(cfg: CFG, ev: dict, val: dict):
     '''Update the map val'''
-    pass
+    change = False
+    return change
 
 def remove_instrs(cfg: CFG, ev: dict, val: dict):
     '''When ev and val are fully updated, removed redundant
@@ -47,6 +67,10 @@ def optimize_sccp(decl: Proc):
     '''Perform sccp for the given declaration'''
     cfg = infer(decl)
     crude_ssagen(decl, cfg)
-    val, ev = initialize_conditions(cfg)
+    ev, val = initialize_conditions(cfg)
+    while max(update_ev(cfg,ev,val), update_val(cfg,ev,val)) :
+        pass
+    
+        
 
 
