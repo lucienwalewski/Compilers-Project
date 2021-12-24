23f5e51c1c756a28e99cac72a8d0c5c92c76ca79
aurelebohbot <aurelemmb@gmail.com>
2021-12-24 18:29:16 +0100

    /

--- src/bx2tac_opt.py
+++ src/bx2tac_opt.py
@@ -14,8 +14,13 @@ import sys
 import argparse
 from tac import Proc
 from sccp import optimize_sccp
+from tac_doft import optimize_decl
 import tac
-
+from parser import *
+from lexer import *
+import bx_ast as ast
+import bx2tac
+import json
 
 
 
@@ -29,12 +34,17 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', dest='output', type=str)
     args = parser.parse_args(sys.argv[1:])
     bx_file = args.bx_file[0]
+    tac_file = bx_file + 'tac.json'
 
     # Lex and parse the input file
-    # FIXME
-    tac_list = []
+    with open(bx_file, 'r') as fp:
+        data = fp.read()
+        AST = parser.parse(data, lexer=lexer)
+        assert AST is not None
+        AST.type_check()
+        tac_list = TACGen(AST.declarations).ship()
 
-    # Once in tac form, perform sccp
+    # Once in tac form, perform sccp + copy propagation
     new_tac_list = []
     print("yo")
     for decl in tac_list:
@@ -42,4 +52,5 @@ if __name__ == '__main__':
         if isinstance(decl, Proc):
             print("test")
             optimize_sccp(decl)
+            optimize_decl(decl)
         new_tac_list.append(decl)
